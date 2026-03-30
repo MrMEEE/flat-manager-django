c0b06e2019f78d33fd55e6a08da6a688f750db13
Martin Juhl Prendergast <m@rtinjuhl.dk>
2026-03-30 16:14:17 +0200

    settings: read TIME_ZONE from env var, default UTC

--- config/settings.py
+++ config/settings.py
@@ -127,7 +127,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 # Internationalization
 LANGUAGE_CODE = 'en-us'
-TIME_ZONE = 'UTC'
+TIME_ZONE = os.environ.get('TIME_ZONE', 'UTC')
 USE_I18N = True
 USE_TZ = True
 
